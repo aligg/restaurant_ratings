0b6778fccaa097ec07cfd7afa7a8307ff5f4c9ba
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-28 18:01:10 -0700

    Added user pick feature

--- ratings.py
+++ ratings.py
@@ -9,28 +9,35 @@
 
 
 # put your code here
-filename = open('scores.txt')
 
 restaurant_ratings = {}
 
-for line in filename:
-    line = line.rstrip()
-    rest_data = line.split(':')
-    rest = rest_data[0]
-
-    if rest not in restaurant_ratings:
-        restaurant_ratings[rest_data[0]] = rest_data[1]
+def create_rest_dict(filename):
+    filename = open(filename)
 
 
-rests = restaurant_ratings.items()
+    for line in filename:
+        line = line.rstrip()
+        rest_data = line.split(':')
+        rest = rest_data[0]
 
-sorted_rests = sorted(rests)
+        if rest not in restaurant_ratings:
+            restaurant_ratings[rest_data[0]] = rest_data[1]
 
-for i in range(len(sorted_rests)):
-    print "%s is rated at %s." % (sorted_rests[i][0], sorted_rests[i][1])
+
+    rests = restaurant_ratings.items()
+
+    sorted_rests = sorted(rests)
+
+    for i in range(len(sorted_rests)):
+        print "%s is rated at %s." % (sorted_rests[i][0], sorted_rests[i][1])
+
+create_rest_dict('scores.txt')
 
 
 def add_rating():
+
+   # restaurant_ratings = create_rest_dict('scores.txt')
 
     while True:
         restaurant_name = raw_input('What is the restaurant\'s name?')
@@ -42,5 +49,19 @@ def add_rating():
         else:
             print "Please enter a number between 1 and 5"
 
-
 add_rating()
+
+
+def user_pick():
+    user_choice = raw_input("Welcome to restaurant ratings. Would you like to: (s)ee all ratings, (a)dd a new rating, or (q)uit?")
+
+    if user_choice == 's':
+        return create_rest_dict('scores.txt')
+    elif user_choice == 'a':
+        return add_rating()
+    elif user_choice == 'q':
+        return
+    else:
+        "Please choose a valid input"
+
+user_pick()
